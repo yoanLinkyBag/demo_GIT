c562843c122d62810b872b7062fdd1bfde96a89c
yoanLinkyBag <47569260+yoanLinkyBag@users.noreply.github.com>
2019-03-16 17:16:20 +0100

    Mise à jour du head

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <html>
 <head>
-	
+	<meta charset="utf-8">
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+	<title>LinkyBag # 1er site de consigne entre particuliers</title>
+	<link rel="stylesheet" href="public/css/style.css">
+	<link rel="icon" type="image/jpeg" href="public/images/favicon.png" />
+	<link rel="apple-touch-icon" href="public/images/favicon.png" />
 </head>
