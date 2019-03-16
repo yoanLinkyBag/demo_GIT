0d300e1acb0907573b67bc2329285db1246f823e
Yoan Le Tiec <yoan.letiec@icloud.com>
2019-03-16 18:20:30 +0100

    Changement du title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-	<title>LinkyBag # 1er site de consigne entre particuliers</title>
+	<title>Ova - Particuliers ou professionnels, nous sommes là pour vous !</title>
 	<link rel="stylesheet" href="public/css/style.css">
 	<link rel="icon" type="image/jpeg" href="public/images/favicon.png" />
 	<link rel="apple-touch-icon" href="public/images/favicon.png" />
